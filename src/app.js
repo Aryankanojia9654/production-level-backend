7ad33ea5709322b80aa1d348070b2ad78f188933
Aryankanojia9654 <aryankanojia354@gmail.com>
2024-10-18 19:12:32 +0530

    backend updated

--- src/app.js
+++ src/app.js
@@ -12,3 +12,8 @@ app.uses(cors({
     origin : process.env.CORS_ORIGIN ,
     Credential: true
 }))
+
+app.use(cookieParser())
+app.use(express.json({limit : '50mb'})) 
+app.use(express.urlencoded({extended : true})) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public')) // for serving static files
